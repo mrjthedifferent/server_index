a4a905e698fb90d90d22a447171e2f5fbe584e98
Md Mahfuzur Rahman <mahfuzur@dana.money>
2025-06-16 16:04:00 +0600

    remove scripts

--- .scripts/features/install_node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🟢 Node.js & npm Installer"
-sudo apt update && sudo apt install -y curl
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt install -y nodejs
-node -v && npm -v
-echo "✅ Node.js and npm installation complete!" 
--- .scripts/features/install_supervisor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🔁 Supervisor Installer"
-sudo apt update && sudo apt install -y supervisor
-sudo systemctl enable supervisor && sudo systemctl start supervisor
-echo "✅ Supervisor installation complete!" 
